46b1088663639ee459afcb2c4a29dbaff2525c99
strehi <freifunk@inti.inka.de>
2015-01-18 23:25:46 +0100

    New stable Version 0.2.0 (based on gluon 2014.4)

--- site.mk
+++ site.mk
@@ -1,15 +1,20 @@
 GLUON_SITE_PACKAGES := \
+        gluon-mesh-batman-adv-14 \
 	gluon-alfred \
 	gluon-announced \
 	gluon-autoupdater \
-	gluon-config-mode \
+        gluon-config-mode-autoupdater \
+        gluon-config-mode-hostname \
+        gluon-config-mode-mesh-vpn \
+        gluon-config-mode-geo-location \
+        gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
+        gluon-luci-private-wifi \
 	gluon-next-node \
-	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-status-page \
@@ -17,7 +22,8 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.1.7~stable$(shell date '+%Y%m%d')
+
+DEFAULT_GLUON_RELEASE := 0.2.0~stable$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
